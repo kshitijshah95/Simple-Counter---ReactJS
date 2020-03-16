6047c46e86c65bbd39c29b84e813cc41ffdf1b19
Kshitij Shah <kshitijshah95@gmail.com>
2020-03-16 09:19:49 -0700

    simple Counter v1.0

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      count: 0,
+    }
+  }
+
+  handleIncreament = () => {
+    this.setState({count: this.state.count + 1});
+  }
+
+  handleDecreament = () => {
+    this.setState({count: this.state.count - 1});
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Simple Counter App</h1>
+        <h3>{ this.state.count }</h3>
+        <button onClick = {this.handleIncreament}>+</button>
+        <button onClick = {this.handleDecreament}>-</button>
+      </div>
+    );
+  }
 }
 
 export default App;
